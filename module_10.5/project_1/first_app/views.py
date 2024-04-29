92bbe6cd9721b122aeb22e5996676f611cba8a1c
Sharmin Akter <spopiq@gmail.com>
2024-04-29 07:12:55 +0600

    added filters

--- module_10.5/project_1/first_app/views.py
+++ module_10.5/project_1/first_app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home(request):
-    data = [
-    {'name': 'zed', 'age': 19},
-    {'name': 'amy', 'age': 22},
-    {'name': 'joe', 'age': 31},
-]
-    return render(request, 'index.html', {'data' : data})
+    data = {'str_list' : ['python', 'is', 'fun'],
+    'dict_list': [{'name': 'Josh', 'age': 19},
+    {'name': 'Dave', 'age': 22},
+    {'name': 'Joe', 'age': 31},
+    ],
+    'date' : datetime.datetime.now(),
+    'value' : 5,
+    'value2':"",
+    'string' : "I'm Django",
+    'string2' : 'python',
+    'string3' : 'HELLO WORLD',
+    'title' : 'django filters'
+    }
+    
+    return render(request, 'index.html', data)
